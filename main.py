a6b7dcc0ad6768a07ad99900f995835e6ee53e9b
John Zou <johnbzou@Johns-MacBook-Air.local>
2026-01-17 14:40:42 -0500

    BANG

---
+++ main.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    from src import * 
+
+    train_dataset, val_dataset, test_dataset = get_sft_datasets()
+
+    model, tokenizer = load_model_and_tokenizer()
+
+    # create training arguments
+    training_args = create_training_arguments()
+
+    # create trainer
+    trainer = create_sft_trainer(
+        model=model,
+        tokenizer=tokenizer,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        training_args=training_args,
+    )
+
+    # train and save
+    train_and_save(trainer)
